24b59d306a6f055a68a3e06b6d13a8f29456ee86
Whatisinternet <joshteeter@gmail.com>
2015-01-06 01:02:56 -0500

    Permitted edits -- lazy fix for error

--- app/controllers/todo.js
+++ app/controllers/todo.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
-export default Ember.Controller.extend({
+export default Ember.ObjectController.extend({
+    actions: {
+        editTodo: function() {
+            this.set('isEditing', true);
+        }
+    }
 });
