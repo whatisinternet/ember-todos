e7124f4faa3d39bfe26f1eee3716c6884eaed7d0
Whatisinternet <joshteeter@gmail.com>
2015-01-06 01:14:11 -0500

    Getting close. Template is now just outlet and default requests

--- app/router.js
+++ app/router.js
@@ -8,6 +8,9 @@ var Router = Ember.Router.extend({
 Router.map(function() {
   this.resource("todos", {
     path: "/"
+  }, function() {
+    this.route("active");
+    this.route("complete");
   });
 });
 
